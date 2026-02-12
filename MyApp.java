66f8d992f739e7294c223aba4f9253aebcd96cc2
Dcaller4E <dcaller4@epcc.edu>
2026-02-12 18:41:22 +0000

    It works

--- MyApp.java
+++ MyApp.java
@@ -1,1 +1,12 @@
 
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("It works!!");
+    }
+}
+
+// javac MyApp.java
+// Compile to binary
+
+// java MyApp
+// run
